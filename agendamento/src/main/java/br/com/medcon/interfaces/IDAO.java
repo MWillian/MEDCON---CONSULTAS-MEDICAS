28789cd453060dbb82c925346354ac5714d78de4
MWillian <matheuswno02@gmail.com>
2025-12-03 03:30:38 -0300

    Implementado o EspecialidadeDAO para acessar o banco de dados. Esta classe deve ser utilizada em conjunto com os BOs, para testar as regras de negócio da aplicação.

---
+++ agendamento/src/main/java/br/com/medcon/interfaces/IDAO.java
@@ -0,0 +1,12 @@
+package br.com.medcon.interfaces;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IDAO<T> {
+    //vamos criar uma interface para todos as classes DAO
+    void salvar(T objeto) throws SQLException;
+    void atualizar(T objeto) throws SQLException;
+    void deletar(int id) throws SQLException;
+    T buscarPorId(int id) throws SQLException;
+    List<T> listarTodos() throws SQLException;
+}
