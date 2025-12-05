e9b01ba1724dd4ad8a996a4f17a7d235904dbb67
Call333 <cam4@aluno.ifal.edu.br>
2025-12-04 22:13:12 -0300

    Adicionado a Interface ISolicitacaoDAO e SolicitacaoDAO

--- agendamento/src/main/java/br/com/medcon/enums/StatusSolicitacao.java
+++ agendamento/src/main/java/br/com/medcon/enums/StatusSolicitacao.java
@@ -1,5 +1,24 @@
 package br.com.medcon.enums;
 
 public enum StatusSolicitacao {
-    AGENDADA, PENDENTE, CANCELADA
+    AGENDADA(0), PENDENTE(1), CANCELADA(2);
+
+    private final int codigo;
+
+    StatusSolicitacao(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+    
+    public static StatusSolicitacao fromCodigo(int codigo) {
+        for (StatusSolicitacao statusSolicitacao : values()) {
+            if(statusSolicitacao.getCodigo() == codigo) {
+                return statusSolicitacao;
+            }
+        }
+        throw new IllegalArgumentException("O Valor invalido: " + codigo);
+    }
 }
